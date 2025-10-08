fe9870c759e5b209503069ce746de1d7c419ac2b
py-20 <mystudy474@gmail.com>
2025-10-08 08:30:17 +0530

    Add files via upload

---
+++ forw.py
@@ -0,0 +1,32 @@
+def u_cal(u, n):
+    temp = u
+    for i in range(1, n):
+        temp = temp * (u - i)
+    return temp
+def fact(n):
+    f=1
+    for i in range(2,n+1):
+        f*=i
+    return f
+n=int(input('enter no of data points'))
+x=np.zeros((n))
+y=np.zeros((n,n))
+print("enter data for x and y:")
+for i in range(n):
+    x[i]=float(input('x['+str(i)+']='))
+    y[i][0]=float(input('y['+str(i)+']='))
+for i in range (1,n):
+    for j in range (0,n-i):
+        y[j][i]=y[j+1][i-1]-y[j][i-1]
+print('\n FORWORD DIFFRENCE TABLE\n')
+for i in range(0,n):
+    print('%0.2f'%(x[i]),end='')
+    for j in range(0,n-i):
+         print('\t\t%0.2f'%(y[i][j]),end='')
+    print()
+value=float(input('value:'))
+sum=y[0][0]
+u=(value-x[0])/(x[1]-x[0])
+for i in range(1,n):
+    sum=sum+(u_cal(u,i)*y[0][i]/fact(i))
+print("\n Interpoluted Value at",value,"is",round(sum,6))
