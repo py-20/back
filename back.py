d93292f255de9c90e4629080e887ae62e22dae10
py-20 <mystudy474@gmail.com>
2025-10-08 08:34:33 +0530

    Update and rename forw.py to back.py

--- back.py
+++ back.py
@@ -1,7 +1,7 @@
 def u_cal(u, n):
     temp = u
     for i in range(1, n):
-        temp = temp * (u - i)
+        temp = temp * (u + i)
     return temp
 def fact(n):
     f=1
@@ -16,17 +16,17 @@ for i in range(n):
     x[i]=float(input('x['+str(i)+']='))
     y[i][0]=float(input('y['+str(i)+']='))
 for i in range (1,n):
-    for j in range (0,n-i):
-        y[j][i]=y[j+1][i-1]-y[j][i-1]
-print('\n FORWORD DIFFRENCE TABLE\n')
+    for j in range (n-1,i-2,-1):
+        y[j][i]=y[j][i-1]-y[j-1][i-1]
+print('\n BACKWORD DIFFRENCE TABLE\n')
 for i in range(0,n):
     print('%0.2f'%(x[i]),end='')
-    for j in range(0,n-i):
+    for j in range(0,i+1):
          print('\t\t%0.2f'%(y[i][j]),end='')
     print()
-value=float(input('value:'))
-sum=y[0][0]
-u=(value-x[0])/(x[1]-x[0])
+value=float(input('Interpoluted value:'))
+sum=y[n-1][0]
+u=(value-x[n-1])/(x[1]-x[0])
 for i in range(1,n):
-    sum=sum+(u_cal(u,i)*y[0][i]/fact(i))
-print("\n Interpoluted Value at",value,"is",round(sum,6))
+    sum=sum+(u_cal(u,i)*y[n-1][i]/fact(i))
+print("\n Interpoluted Value at",value,"is",sum)
